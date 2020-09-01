79248915b578984b5c6cac9c52eb8540bb4a5236
vijaypauniyan <70579978+vijaypauniyan@users.noreply.github.com>
2020-09-01 17:11:34 +0530

    Fist Commit api
    First ommmoojjd

---
+++ MedicineAppApi/MedicineAppApi/Controllers/MedicineController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using MedicineAppApi.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicineAppApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicineController : ControllerBase
+    {
+        static List<Medicine> list = new List<Medicine>();
+        private List<Medicine> MedicineAll()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Medicine medicine = new Medicine()
+                {
+                    Brand = "Brand " + i,
+                    ExpiryDate = DateTime.Now.AddDays(i),
+                    FullName = "Ankit " + i,
+                    Note = "Note " + i,
+                    Price = +i * 10,
+                    Quantity = i,
+                    MedicineId = i
+                };
+                list.Add(medicine);
+            }
+            return list;
+        }
+        //IEnumerable<Medicine> list = new List<Medicine>();
+        [HttpGet]
+        public List<Medicine> Get()
+        {
+            return MedicineAll();
+        }
+        [HttpPut]
+        public Medicine UpdateMedicine([FromQuery] int medicineid, [FromBody] Medicine medicine)
+        {
+            Medicine medicine1 = list.Where(e => e.MedicineId == medicineid).FirstOrDefault();
+            if (medicine1 != null)
+            {
+                list.Remove(medicine1);
+                list.Add(medicine);
+            }
+            return medicine;
+        }
+        [HttpPost]
+        public Medicine AddMedicine([FromBody] Medicine medicine)
+        {
+            list.Add(medicine);
+            return medicine;
+        }
+
+        [HttpGet]
+        [Route("GetmedicineById")]
+        public Medicine GetmedicineById([FromQuery] int medicineid)
+        {
+            return list.Where(e => e.MedicineId == medicineid).FirstOrDefault();
+        }
+    }
+}
---
+++ MedicineAppApi/MedicineAppApi/Model/Medicine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicineAppApi.Model
+{
+    public class Medicine
+    {
+        public int MedicineId { get; set; }
+        public string FullName { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public string Note { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public int Quantity { get; set; }
+    }
+}
